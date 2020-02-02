74bf0149abb1c4f1bbc939b51276e17057fbbeca
Samar Omar <samaromm98@gmail.com>
2020-02-02 20:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const farmAnimals = 'cow horse sheep pig chicken'
-let [moo,neigh,]=farmAnimals.split(' ')
+let [moo,neigh,baa,]=farmAnimals.split(' ')
 
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
